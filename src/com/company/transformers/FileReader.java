17a233aaf9ee78c05e6e785ce1b8e2b9f28f34cb
vitodn <51167572+vitodn@users.noreply.github.com>
2019-06-18 09:50:23 +0200

    text

--- src/com/company/transformers/FileReader.java
+++ src/com/company/transformers/FileReader.java
@@ -1,22 +1,31 @@
 package com.company.transformers;
 import com.sun.org.apache.xpath.internal.operations.String;
 import jdk.jshell.spi.Execution
+
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileReader implements StringTransformer
 
 {
 
     @Override
-    public string [] transform(String[] input) {
+    public string [] transform(String[] input) throws IOException
+    {
 
         var fileContents = new String() [input.length];
 
         for (int= i = 0; i<input.length; i++) {
+            try {
 
-            vsr = content = readFile (input [i]);
+            var content = Files.readAllLines(Paths.get(input[i]));
             fileContents [i] = content;
+        } catch (IOException ex) {
+                System.out.println("Error: " + ex.getMessage());
+                fileContents[i] = "";
 
-        }
+
+            }
     }
 }
